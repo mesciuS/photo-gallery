1c1e1929cce70e1127561037ea46939dd6977c79
Giovanni <giovadev@gmail.com>
2023-10-26 13:05:16 +0200

    feat: style toggler

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -7,22 +7,22 @@ export default {
   },
 
   mounted() {
-   
+    
   },
 };
 </script>
 
 <template>
-  <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary" data-bs-theme="light">
+  <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
       <div>
-        <router-link class="navbar-brand" href="#" :to="{name: 'home'}"><img src="../assets/images/Adventure.png" alt=""></router-link>
+        <router-link class="navbar-brand" href="#" :to="{name: 'home'}"><img id="nav-logo" src="../assets/images/Adventure.png" alt=""></router-link>
       </div>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
+          <ul class="navbar-nav nav-underline">
             <li class="nav-item">
               <router-link class="nav-link" aria-current="page" href="#" :to="{name: 'gallery'}">Gallery</router-link> 
             </li>
@@ -50,7 +50,13 @@ export default {
   //     display: none;
   //   }
   // }
-  img {
+  #nav-logo {
     height: 80px;
+  }
+  .navbar-collapse {
+    transition: all 1s;
+  }
+  .navbar-toggler:focus {
+    box-shadow: none;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import App from './App.vue'
 import './Style.scss'
 import {router} from './router';
 import * as bootstrap from 'bootstrap';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+// library.add(faUserSecret);
 
 const app = createApp(App)
 
 app.use(router)
-
+// app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
 
