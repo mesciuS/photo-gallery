dc50f7505fe505a50918922b90a4ac1acf64f0a4
Giovanni <giovadev@gmail.com>
2023-11-09 11:44:33 +0100

    feat: scaffolding footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,12 +9,32 @@ export default {
 </script>
 
 <template>
-  <footer id="footer">Sono il footer</footer>
+  <footer id="footer">
+    <div class="footer-square">
+      <h1>ciao</h1>
+      <h2>ciao 2</h2>
+    </div>
+    <div class="footer-square">
+      <h1>ciao</h1>
+      <h2>ciao 2</h2>
+    </div>
+    <div class="footer-square">
+      <h1>ciao</h1>
+      <h2>ciao 2</h2>
+    </div>
+  </footer>
 </template>
 
 <style scoped lang="scss">
 #footer {
+  display: flex;
+  justify-content: space-around;
   height: 200px;
+  padding: 20px;
   background-color: #333333;
+} .footer-square {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
