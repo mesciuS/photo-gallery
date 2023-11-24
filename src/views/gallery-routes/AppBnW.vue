efb05473de9a51c34d8f2cfa13cf257779f16ed2
Giovanni <giovadev@gmail.com>
2023-11-24 10:31:19 +0100

    feat: struttura gallery-view

--- src/views/gallery-routes/AppBnW.vue
+++ src/views/gallery-routes/AppBnW.vue
@@ -5,10 +5,46 @@ export default {
 
 </script>
 <template>
-    <div>
-        bnw
+    <div id="gallery-view-wrapper">
+        <h2 id="gallery-view-title">NATURE IN BLACK AND WHITE</h2>
+        <div id="gallery-view">
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+            <div class="view-img"></div>
+        </div>
     </div>
 </template>
 <style scoped lang="scss">
-    
+    #gallery-view-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        min-height: calc(100vh - 106px);
+    }
+    #gallery-view-title {
+        font-size: 3em;
+    }
+    #gallery-view{
+        display: flex;
+        flex-wrap: wrap;
+        max-width: 1030px;
+        gap: 20px;
+    } .view-img {
+        min-width: 190px;
+        min-height: 150px;
+        background-color: aqua;
+    }
 </style>
